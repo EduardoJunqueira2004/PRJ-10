974d0b5e1a17e649e1c11c47744a59cce2bef2db
Eduardo Miguel Moreira Junqueira <eduardo.j@ipvc.pt>
2024-05-28 22:07:58 +0100

    Updatefinal
    final

--- Octave/aplicar_efeito.m
+++ Octave/aplicar_efeito.m
@@ -1,19 +1,52 @@
-function sinal_processado = aplicar_efeito(sinal, fa, escolha, parametros)
-    % Selecionar o efeito com base na escolha do utilizador
-    while true
-        switch escolha
-            case 1
-                sinal_processado = aplicar_wahwah(sinal, fa, parametros);
-                break
-            case 2
-                sinal_processado = aplicar_tremolo(sinal, fa, parametros);
-                break
-            case 3
-                sinal_processado = aplicar_reverb(sinal, fa, parametros);
-                break
-            otherwise
-                % Se a escolha for inválida, pedir uma nova entrada
-                escolha = input('Escolha inválida. Por favor, escolha 1, 2 ou 3: ');
-        end
+function sinal_processado = aplicar_efeito(sinal, fa, escolha, parametros, impulso)
+    switch escolha
+        case 1 % Wahwah
+            sinal_processado = aplicar_wahwah(sinal, fa, parametros);
+        case 2 % Tremolo
+            sinal_processado = aplicar_tremolo(sinal, fa, parametros);
+        case 3 % Reverb
+            if isempty(impulso)
+                error('Impulso não carregado para o efeito Reverb.');
+            end
+            sinal_processado = aplicar_reverb(sinal, fa, impulso);
+        otherwise
+            error('Escolha de efeito inválida.');
     end
 end
+
+function sinal_processado = aplicar_wahwah(sinal, fa, parametros)
+    freq_central = parametros(1);
+    banda_passagem = parametros(2);
+    largura_banda = 0.1 * freq_central;
+    f_max = 1 - banda_passagem / 2;
+    f_min = banda_passagem / 2;
+    f = f_min + (f_max - f_min) * (1 + sin(2 * pi * freq_central * (0:length(sinal) - 1) / fa)) / 2;
+    f_corte_inf = f - largura_banda / 2;
+    f_corte_sup = f + largura_banda / 2;
+    sinal_processado = zeros(size(sinal));
+    for i = 1:size(sinal, 2)
+        sinal_processado(:, i) = filtro_passe_banda(sinal(:, i), fa, f_corte_inf(i), f_corte_sup(i));
+    end
+    sinal_processado = normalize(sinal_processado, 'range');
+end
+
+function sinal_filtrado = filtro_passe_banda(sinal, fa, f_corte_inf, f_corte_sup)
+    [b, a] = butter(2, [f_corte_inf f_corte_sup] / (fa / 2), 'bandpass');
+    sinal_filtrado = filter(b, a, sinal);
+end
+
+function sinal_processado = aplicar_tremolo(sinal, fa, parametros)
+    % Implementar o efeito Tremolo
+    % ...
+end
+
+function sinal_processado = aplicar_reverb(sinal, fa, impulso)
+    % Aplicar a convolução para cada canal separadamente
+    sinal_processado = zeros(size(sinal));
+    for i = 1:size(sinal, 2)
+        sinal_processado(:, i) = conv(sinal(:, i), impulso, 'same');
+    end
+    % Normalizar o sinal processado
+    sinal_processado = normalize(sinal_processado, 'range');
+end
+
